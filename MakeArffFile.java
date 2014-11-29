0f58a855f259017338a23260606cc4e36077143b
Beth Kieler <bakieler@gmail.com>
2014-11-29 17:14:06 -0600

    Takes RGB codes and counts them into correct bin.  Need to write to a file yet.

--- MakeArffFile.java
+++ MakeArffFile.java
@@ -7,40 +7,95 @@ package MakeArffFile;
 import java.*;
 import java.io.*;
 import java.util.*;
+import java.math.*;
+import java.lang.*;
 
 public class MakeArffFile {
 	
 
 	public static void main(String[] args) {
-		int bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8;
-		String pictureClass; 
-		//int[] colors;
+		int bin1 = 0, bin2 = 0, bin3 = 0, bin4 = 0, bin5 = 0, bin6 = 0, bin7 = 0, bin8 = 0;
+		String pictureClass;
+		int red, green, blue;
+		
+		String[] colors;
 		
 		//open file for reading
-		List<Integer> colors = new ArrayList<Integer>();
-		String[] colors1;
-		int[] colors2 = new int[3];
-		
 		File directory= new File("C:\\Users\\bt5\\Documents\\1-School and cert\\CSC 573 Data Mining\\Final Project\\project_images\\project images\\ztextfilesonly\\airplane\\training");
 		for (File file : directory.listFiles())
 		{
+			System.out.println("\n opening new file \n");
+			bin1 = 0; 
+			bin2 = 0;
+			bin3 = 0;
+			bin4 = 0; 
+			bin5 = 0;
+			bin6 = 0;
+			bin7 = 0;
+			bin8 = 0;
+			
+			//String fileName = file;
+			
+			System.out.println(file);
 		   BufferedReader reader = null;
 			try {
-			    reader = new BufferedReader(new FileReader(file));
-			   // int i = 0;
+				//read file
+			    
+				reader = new BufferedReader(new FileReader(file));
 			    String text = null;
-	int k = 1;
+			    
 			    while ((text = reader.readLine()) != null) {
-			    	System.out.println("been through while " + k + " times.");
-			    	k++;
-			    	System.out.println(text);
-	
-			    	colors1 = text.split("[,]+");
-			    	for (int i = 0; i < colors1.length; i++){
-			    		colors2[i] = Integer.parseInt(colors1[i]);
-			    		System.out.println(colors2[i]);
-			    		
+			    	//parse the line read from the file
+			    	colors = text.split("[,]+");
+			    	red = Integer.parseInt(colors[0]);
+			    	blue = Integer.parseInt(colors[1]);
+			    	green = Integer.parseInt(colors[2]);
+			    
+
+			    	System.out.println("red:  " + red + ";  green:  " + green + ";  blue:  " + blue);	
+			    	double e = 2;
+			    	double f = 7;
+			    	System.out.println(Math.pow(e,f));
+			    	
+			    	if ((red >= 0) && (red < 128) && (green >= 0) && (green < 128) && (blue >= 0) && (blue < 128)){
+			    		bin1++;	    					    		
 			    	}
+			    	else if((red >= 0) && (red < 128) && (green >= 0) && (green < 128) && (blue >= 128) && (blue < 256)){
+			    		bin2++;
+			    	}
+			    	else if((red >= 0) && (red < 128) && (green >= 128) && (green < 256) && (blue >= 0) && (blue < 128)){
+			    		bin3++;
+			    	}			    	
+			    	else if((red >= 0) && (red < 128) && (green >= 128) && (green < 256) && (blue >= 128) && (blue < 256)){
+			    		bin4++;
+			    	}
+			    	else if((red >= 128) && (red < 256) && (green >= 0) && (green < 128) && (blue >= 0) && (blue < 128)){
+			    		bin5++;
+			    	}
+			    	else if((red >= 128) && (red < 256) && (green >= 0) && (green < 128) && (blue >= 128) && (blue < 256)){
+			    		bin6++;
+			    	}			    	
+			    	else if((red >= 128) && (red < 256) && (green >= 128) && (green < 256) && (blue >= 0) && (blue < 128)){
+			    		bin7++;
+			    	}			    	
+			    	else if((red >= 128) && (red < 256) && (green >= 128) && (green < 256) && (blue >= 128) && (blue < 256)){
+			    		bin8++;
+			    	}
+			    
+			    	else {
+			    		System.out.println("FAILED BIN ASSIGNMENT");
+			  		  	System.out.println("\n\nPress num to continue..");
+						Scanner sc = new Scanner(System.in);
+						int i = sc.nextInt();
+						    	
+			    	}
+			    	
+			    	
+			    	System.out.println("bin1:  " + bin1 + "\nbin2:  "+ bin2 + "\nbin3:  " + bin3 + "\nbin4:  " + bin4 + 
+			    			"\nbin5:  " + bin5 + "\nbin6:  " + bin6 + "\nbin7:  " + bin7 + "\nbin8:  " + bin8);
+			    	
+		
+			    	
 			    }
 			} catch (FileNotFoundException e) {
 			    e.printStackTrace();
